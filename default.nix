22ce818b2f1c31654352be487b2ce88d28267b8d
Inom <3938577+Inom-Turdikulov@users.noreply.github.com>
2023-07-22 23:07:30 +0600

    Update bootloader

--- default.nix
+++ default.nix
@@ -55,9 +55,15 @@ with lib.my;
   boot = {
     kernelPackages = mkDefault pkgs.linuxKernel.packages.linux_5_18;
     loader = {
-      efi.canTouchEfiVariables = mkDefault true;
-      systemd-boot.configurationLimit = 10;
-      systemd-boot.enable = mkDefault true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+      };
+      grub = {
+         efiSupport = true;
+         #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+         device = "nodev";
+      };
     };
   };
 
