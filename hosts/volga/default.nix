8a8d3c9708c5b4ead944dafc53516c51aed81247
Inom Turdikulov <inom@iturdikulov.com>
2023-07-23 13:49:02 -0400

    Disable vaulwarden (not used/has activation unit issue)

--- hosts/volga/default.nix
+++ hosts/volga/default.nix
@@ -52,7 +52,7 @@
     };
     shell = {
       adl.enable = true;
-      vaultwarden.enable = true;
+      vaultwarden.enable = false;
       direnv.enable = true;
       git.enable    = true;
       gnupg.enable  = true;
