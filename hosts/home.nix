d028ec6c7114f8661bad614caebee685f61e1ded
Inom Turdikulov <inom@iturdikulov.com>
2023-07-28 17:21:54 -0400

    Adjust time settings

--- hosts/home.nix
+++ hosts/home.nix
@@ -8,17 +8,11 @@ in {
     192.168.1.1   router.home
 
     # Hosts
-    ${optionalString (config.time.timeZone == "Europe/Copenhagen") ''
-        192.168.1.28  ao.home
-        192.168.1.20  murasaki.home
-        192.168.1.19  shiro.home
+    ${optionalString (config.time.timeZone == "Europe/Moscow") ''
+        192.168.1.10  volga.home
       ''}
-    ${optionalString (config.time.timeZone == "America/Toronto") ''
-        192.168.1.2   ao.home
-        192.168.1.3   kiiro.home
-        192.168.1.10  kuro.home
-        192.168.1.11  shiro.home
-        192.168.1.12  midori.home
+    ${optionalString (config.time.timeZone == "Asia/Almaty") ''
+        192.168.1.10  volga.home
       ''}
 
     # Block garbage
@@ -26,15 +20,15 @@ in {
   '';
 
   ## Location config -- since Toronto is my 127.0.0.1
-  time.timeZone = mkDefault "America/Toronto";
+  time.timeZone = mkDefault "Asia/Almaty";
   i18n.defaultLocale = mkDefault "en_US.UTF-8";
   # For redshift, mainly
-  location = (if config.time.timeZone == "America/Toronto" then {
-    latitude = 43.70011;
-    longitude = -79.4163;
-  } else if config.time.timeZone == "Europe/Copenhagen" then {
-    latitude = 55.88;
-    longitude = 12.5;
+  location = (if config.time.timeZone == "Asia/Almaty" then {
+    latitude = 43.238949;
+    longitude = 76.889709;
+  } else if config.time.timeZone == "Europe/Moscow" then {
+    latitude = 53.20066;
+    longitude = 45.00464
   } else {});
 
   # So the vaultwarden CLI knows where to find my server.
