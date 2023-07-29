e1fbeb180019152700bfe8307136d6809854b744
Inom T <i@inomoz.ru>
2023-07-29 14:20:27 +0600

    Add new spotify tui

--- modules/desktop/media/spotify.nix
+++ modules/desktop/media/spotify.nix
@@ -18,6 +18,7 @@ in {
       # still occasionally need the official client for more sophisticated
       # search and the "made for you" playlists.
       spotify
+      spotify-player  # TUI for spotify
 
       # # services.spotifyd doesn't work so we'll have to roll our own spotifyd +
       # # spotify-tui solution. The dbus interface doesn't work, though, so we
