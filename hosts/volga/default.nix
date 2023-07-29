de1bf715c4e4e310411e7932a80faa087e781f7b
Inom Turdikulov <inom@iturdikulov.com>
2023-07-29 22:34:45 +0600

    Fix merge conflict

--- hosts/volga/default.nix
+++ hosts/volga/default.nix
@@ -67,12 +67,9 @@
       xh.enable     = true;
       pass.enable   = true;
       w3m.enable    = true;
-      tealdeer.enable  = true;
-<<<<<<< HEAD
-      vaultwarden.enable = false;
-=======
       ttyper.enable = true;
->>>>>>> 5d3e653 (Add ttyper shell keybord tutor)
+      tealdeer.enable  = true;
+      vaultwarden.enable = false;
     };
     services = {
       dictd.enable = true;
