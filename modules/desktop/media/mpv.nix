236a0bff4fdcc7bb308a45867f7630853e8fd107
Inom Turdikulov <inom@iturdikulov.com>
2023-07-31 02:53:41 +0600

    Fix mpv integration/add custom configuration

--- modules/desktop/media/mpv.nix
+++ modules/desktop/media/mpv.nix
@@ -3,6 +3,7 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.media.mpv;
+    configDir = config.dotfiles.configDir;
 in {
   options.modules.desktop.media.mpv = {
     enable = mkBoolOpt false;
@@ -10,17 +11,26 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      mpv
-      mpvc  # CLI controller for mpv
-
       # Web-torrent integration
       jq
       xidel
       nodePackages.webtorrent-cli
-      mpvScripts.webtorrent-mpv-hook
 
-      # Youtube integration (for hook)
-      yt-dlp
+      (mpv.override {
+        youtubeSupport = true;
+        scripts = [
+          mpvScripts.mpris
+          mpvScripts.webtorrent-mpv-hook
+          mpvScripts.uosc
+        ];
+      })
+
+      mpvc  # CLI controller for mpv
     ];
+
+    home.configFile = {
+      "mpv/mpv.conf".source   = "${configDir}/mpv/mpv.conf";
+      "mpv/input.conf".source = "${configDir}/mpv/input.conf";
+    };
   };
 }
