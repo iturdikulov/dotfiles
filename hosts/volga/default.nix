3f8b8bf7a559305fe723788388a77233e873da20
Inom Turdikulov <inom@iturdikulov.com>
2023-08-07 04:28:45 +0600

    Add telegram

--- hosts/volga/default.nix
+++ hosts/volga/default.nix
@@ -13,6 +13,7 @@
         rofi.enable = true;
         godot.enable = true;
         slack.enable = true;
+        telegram.enable = true;
         pidgin.enable = true;
       };
       browsers = {
