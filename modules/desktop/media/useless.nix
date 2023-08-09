4df26e08cc9ccbb4eeb0a5a67a8939cd6881e84d
Inom Turdikulov <inom@iturdikulov.com>
2023-08-09 15:09:35 +0600

    Add more useless programs

--- modules/desktop/media/useless.nix
+++ modules/desktop/media/useless.nix
@@ -11,13 +11,15 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      cmatrix    # Simulates the falling characters theme from The Matrix movie
-      neo-cowsay # Generates ASCII pictures of a cow with a message
-      fortune    # Displays a pseudorandom message from a database of quotations
-      lolcat     # Rainbows and unicorns
-      figlet     # Program for making large letters out of ordinary text
-      boxes      # Command line ASCII boxes unlimited!
-      sl         # Steam Locomotive runs across your terminal when you type 'sl'
+      cmatrix     # Simulates the falling characters theme from The Matrix movie
+      neo-cowsay  # Generates ASCII pictures of a cow with a message
+      fortune     # Displays a pseudorandom message from a database of quotations
+      lolcat      # Rainbows and unicorns
+      figlet      # Program for making large letters out of ordinary text
+      boxes       # Command line ASCII boxes unlimited!
+      sl          # Steam Locomotive runs across your terminal when you type 'sl'
+      doge        # WoW very terminal doge
+      linux_logo  # Prints an ASCII logo and some system info
     ];
   };
 }
