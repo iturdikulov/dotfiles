b16121eca81b41126f0ce028d426fe3729eaee43
Inom Turdikulov <inom@iturdikulov.com>
2023-08-30 04:16:26 +0600

    Dicitonary integration

--- modules/shell/sdcv.nix
+++ modules/shell/sdcv.nix
@@ -4,13 +4,17 @@ with lib;
 with lib.my;
 let cfg = config.modules.shell.sdcv;
 in {
+  options.modules.shell.sdcv = with types; {
+    enable   = mkBoolOpt false;
+  };
+
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       sdcv
     ];
 
     env = {
-      STARDICT_DATA_DIR = "$XDG_CONFIG_HOME/Reference/dictionary";
+      STARDICT_DATA_DIR = "$HOME/Reference/dictionary";
     };
   };
 }
