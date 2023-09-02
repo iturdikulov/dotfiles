b6b1ad2b7972c4b101dc63d67de8e9da75e94023
Inom Turdikulov <i@inomoz.ru>
2023-09-02 15:48:34 +0600

    Update secrets

--- hosts/volga/secrets/secrets.nix
+++ hosts/volga/secrets/secrets.nix
@@ -1,5 +1,5 @@
-let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMAuq7AMP3tGbkgRpdb0MX4JHT9NeUiif0w8xF00iqjq Inom M. Turdikulov inom@iturdikulov.org";
+let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ+98UauMXllELyhSNhTTJPITI2OmJSNf1HUXxjiv6V";
 in {
-  "wireguard.age".publicKeys = [key];
+  #"wireguard.age".publicKeys = [key];
   "sshcontrol.age".publicKeys = [key];
 }
