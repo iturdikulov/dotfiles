9f175b4187032c1353ea35bc2057b2cfeafb5537
Inom Turdikulov <i@inomoz.ru>
2023-09-18 03:26:35 +0600

    Setup calibre server

--- modules/services/calibre.nix
+++ modules/services/calibre.nix
@@ -10,7 +10,8 @@ in {
 
   config = mkIf cfg.enable {
     services.calibre-server.enable = true;
-
+    services.calibre-server.libraries = [ "${config.user.home}/Arts_and_Entertainment/literature" ];
     networking.firewall.allowedTCPPorts = [ 8080 ];
+    services.calibre-server.user = "${config.user.name}";
   };
 }
