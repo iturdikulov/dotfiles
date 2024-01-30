aae8d743e9d4efa5a7cd97adef6c52a00ae76a35
Inom Turdikulov <i@inomoz.ru>
2024-01-30 23:23:34 +0600

    Add new formatter packages and prettier config

---
+++ modules/dev/formatters.nix
@@ -0,0 +1,23 @@
+# modules/dev/formatters.nix
+#
+# My various formatters utilites
+
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let devCfg = config.modules.dev;
+    cfg = devCfg.formatters;
+in {
+  options.modules.dev.formatters = {
+    enable = mkBoolOpt false;
+  };
+
+  config = {
+    user.packages = with pkgs; [
+      ruff
+      prettierd
+      deno
+    ];
+  };
+}
