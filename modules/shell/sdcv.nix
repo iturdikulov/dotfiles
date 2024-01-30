a044c0cfad7b1256dc3aea6dd6f81590f042d500
Inom Turdikulov <i@inomoz.ru>
2024-01-30 22:37:26 +0600

    Better sort dictionaries

--- modules/shell/sdcv.nix
+++ modules/shell/sdcv.nix
@@ -16,5 +16,29 @@ in {
     env = {
       STARDICT_DATA_DIR = "$HOME/Reference/dictionary";
     };
+
+    home.file.".sdcv_ordering" = {
+      text = ''
+        Wiktionary (En-Ru)
+        LingvoUniversal (En-Ru)
+        Abbrev
+        LingvoComputer (En-Ru)
+        New Oxford American Dictionary. Angus Stevenson and Christine A. Lindberg (En-En)
+        WordNet
+        LingvoComputer (Ru-En)
+        LingvoUniversal (Ru-En)
+        TechAbrreviations (Ru-Ru)
+        Словарь русского языка. Unknown (Ru-Ru)
+        Большой словарь иностранных слов (Ru-Ru)
+        Новый словарь русского языка. Толково-словообразовательный. Ефремова Т.Ф. (Ru-Ru)
+        Толковый словарь русского языка. С.И.Ожегов и Н.Ю.Шведова (Ru-Ru)
+        Толковый словарь русского языка. Д. Н. Ушаков (Ru-Ru)
+        Этимологический Словарь. Семенов А.В (Ru-Ru)
+        Этимологический словарь русского языка. М. Фасмер (Ru-Ru)
+        Толковый словарь живого великорусского языка. В. Даль (Ru-Ru)
+        Wiktionary (Ru-Ru)
+        Wiktionary (Ru-En)
+        '';
+    };
   };
 }
