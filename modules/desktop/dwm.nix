bce88d2b69713a41fac0a5a6f7b4a551789d1e58
Inom Turdikulov <i@inomoz.ru>
2024-04-06 00:39:54 +0600

    Add high dpi configuration

--- modules/desktop/dwm.nix
+++ modules/desktop/dwm.nix
@@ -113,6 +113,13 @@ in
       gvfs.enable = true;
     };
 
+    # High dpi
+    services.xserver.dpi = 180;
+    environment.variables = {
+      GDK_SCALE = "2";
+      GDK_DPI_SCALE = "0.5";
+      _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+    };
 
     systemd = {
       user.services.polkit-gnome-authentication-agent-1 = {
