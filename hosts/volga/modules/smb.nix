5ff8efff5a554df12a7b8133b1aefd618c45f00f
Inom Turdikulov <i@inomoz.ru>
2024-05-29 19:45:38 +0300

    Add baikal smb integration

--- hosts/volga/modules/smb.nix
+++ hosts/volga/modules/smb.nix
@@ -21,6 +21,14 @@
 
     in ["${automount_opts},credentials=${config.age.secrets.samba.path},uid=1000,gid=100"];
   };
+  fileSystems."/mnt/share/baikal/prj" = {
+    device = "//baikal/prj";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},uid=1000,gid=100"];
+  };
 
   # Server
   services.samba = {
