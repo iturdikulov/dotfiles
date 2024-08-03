9a66e0afaebad7f1e4a8a78cc3dd741c367b05a9
Inom Turdikulov <i@inomoz.ru>
2024-08-03 10:03:55 +0300

    Adjust steam configuration

--- modules/desktop/gaming/steam.nix
+++ modules/desktop/gaming/steam.nix
@@ -3,7 +3,6 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.gaming.steam;
-    steamDir = "$XDG_RUNTIME_HOME/steam";
 in {
   options.modules.desktop.gaming.steam = with types; {
     enable = mkBoolOpt false;
@@ -20,19 +19,16 @@ in {
     environment.systemPackages = with pkgs; [
       (writeScriptBin "steam" ''
         #!${stdenv.shell}
-        HOME="${steamDir}" exec ${pkgs.gamemode}/bin/gamemode ${config.programs.steam.package}/bin/steam "$@"
+        exec ${pkgs.gamemode}/bin/gamemode ${config.programs.steam.package}/bin/steam "$@"
       '')
       # for running GOG and humble bundle games
       (writeScriptBin "steam-run" ''
         #!${stdenv.shell}
-        HOME="${steamDir}" exec ${pkgs.gamemode}/bin/gamemode ${config.programs.steam.package.run}/bin/steam-run "$@"
+        exec ${pkgs.gamemode}/bin/gamemode ${config.programs.steam.package.run}/bin/steam-run "$@"
       '')
     ] ++ (if cfg.mangohud.enable then [ pkgs.mangohud ] else []);
 
     # better for steam proton games
     systemd.extraConfig = "DefaultLimitNOFILE=1048576";
-
-    # Potentially useful packages
-    # gamemode require for some games (like BeamNG.drive)
   };
 }
