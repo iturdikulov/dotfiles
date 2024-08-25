fcca0bce4c004a479be10eee7dc421afef2ca950
Inom Turdikulov <i@inomoz.ru>
2024-08-25 11:51:27 +0300

    Improve tailscale up process

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -1,4 +1,4 @@
-{ options, config, lib, ... }:
+{ options, config, pkgs, lib, ... }:
 
 with lib;
 with lib.my;
@@ -16,6 +16,42 @@ in {
     services.tailscale = {
       enable = true;
       useRoutingFeatures = cfg.routingFeatures;
+    };
+
+    # Create a oneshot job to authenticate to Tailscale
+    systemd.services.tailscale-autoconnect = {
+      description = "Automatic connection to Tailscale";
+
+      # Make sure tailscale is running before trying to connect to tailscale
+      after = [ "network-pre.target" "tailscale.service" ];
+      wants = [ "network-pre.target" "tailscale.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      # Set this service as a oneshot job
+      serviceConfig.Type = "oneshot";
+
+      # Have the job run this shell script
+      script = with pkgs; ''
+        # wait for tailscaled to settle
+        sleep 2
+
+        # check if we are already authenticated to tailscale
+        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
+
+        # otherwise authenticate with tailscale
+        ${tailscale}/bin/tailscale up -authkey tskey-examplekeyhere
+      '';
+    };
+
+    networking.firewall = {
+      # Always allow traffic from your Tailscale network
+      trustedInterfaces = [ "tailscale0" ];
+
+      # Allow the Tailscale UDP port through the firewall
+      allowedUDPPorts = [ config.services.tailscale.port ];
     };
   };
 }
