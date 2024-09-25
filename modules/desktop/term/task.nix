6710229f0bc1764e968c4e2a3cc15928313ab228
Inom Turdikulov <i@inomoz.ru>
2024-09-25 17:46:09 +0300

    Improve taskwarrior notification service

--- modules/desktop/term/task.nix
+++ modules/desktop/term/task.nix
@@ -21,6 +21,8 @@ in {
     home.configFile."task/taskrc".source = "${configDir}/task/taskrc";
     modules.shell.zsh.rcFiles = [ "${configDir}/task/aliases.zsh" ];
 
+    # NEXT: wrap into own nix module
+    # Notify me when timewarrior is tracking
     systemd.timers."timew_track" = {
       wantedBy = [ "timers.target" ];
       timerConfig = {
@@ -35,7 +37,7 @@ in {
           # DBUS is optimized for dwm-desktop, you need to tune it for your DE
           export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $USER)/bus
           export DISPLAY=:0
-          $(${pkgs.timewarrior}/bin/timew|grep Tracking && ${pkgs.libnotify}/bin/notify-send --expire-time=1000 "Timewarrior" "Tracking is active") || exit 0
+          $(${pkgs.timewarrior}/bin/timew|grep Tracking && ${pkgs.libnotify}/bin/notify-send --urgency=low --expire-time=1000 "Timewarrior" "Tracking is active") || exit 0
       '';
       serviceConfig = {
         Type = "oneshot";
