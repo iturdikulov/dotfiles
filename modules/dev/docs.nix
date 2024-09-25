214070412d114fc8f95aefdaaec8d1f5faed21d9
Inom Turdikulov <i@inomoz.ru>
2024-09-25 17:44:24 +0300

    Drop dasht and add kiwix

--- modules/dev/docs.nix
+++ modules/dev/docs.nix
@@ -16,12 +16,9 @@ in {
     # and manpages. You can add those to your system like this
     documentation.dev.enable = true;
 
-    # dasht docsets path
-    env.DASHT_DOCSETS_DIR = "${config.user.home}/Reference/docsets";
-
     user.packages = with pkgs; [
-      dasht    # to search in docsets from CLI
-      zeal     # GUI for docsets, if need to render images
+      unstable.zeal     # GUI for docsets, if need to render images
+      kiwix              # Wiki offline reader
       tealdeer # Simplified, example based and community-driven man pages.
       man-pages
       man-pages-posix
