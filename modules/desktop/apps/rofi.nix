3e7be1dd155e149e95994760d0319c3f4bd5f1c9
Inom Turdikulov <i@inomoz.ru>
2024-09-29 08:20:53 +0300

    Optimize rofi config for wayland

--- modules/desktop/apps/rofi.nix
+++ modules/desktop/apps/rofi.nix
@@ -3,6 +3,12 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.apps.rofi;
+    rofiPkg = if config.modules.desktop.type == "wayland"
+                then pkgs.rofi-wayland-unwrapped
+                else pkgs.rofi-unwrapped;
+    rofiCalcPkg = pkgs.rofi-calc.override { rofi-unwrapped = rofiPkg; };
+    rofiFBPkg = pkgs.rofi-file-browser.override { rofi = rofiPkg; };
+    configDir = config.dotfiles.configDir;
 in {
   options.modules.desktop.apps.rofi = {
     enable = mkBoolOpt false;
@@ -13,13 +19,23 @@ in {
 
     environment.variables.ROFI_PLUGIN_PATH = [
       "$XDG_CONFIG_HOME/rofi/plugins"  # for local development
-      "${pkgs.rofi-calc}/lib/rofi"
-      "${pkgs.rofi-file-browser}/lib/rofi"
+      "${rofiCalcPkg}/lib/rofi"
+      "${rofiFBPkg}/lib/rofi"
     ];
 
-    user.packages = with pkgs; [
-      rofi-unwrapped
+    home.configFile = {
+       "rofi/theme" = { source = "${configDir}/rofi"; recursive = true; };
+       "rofi/config.rasi".source = "${configDir}/rofi_config.rasi";
+    };
 
+    user.packages = with pkgs; [
+      rofiPkg
+      (makeDesktopItem {
+        name = "rofi-open-file";
+        desktopName = "Open File ->";
+        icon = "folder";
+        exec = "${rofiPkg}/bin/rofi -show filebrowser";
+      })
       # Fake rofi dmenu entries
       (makeDesktopItem {
         name = "rofi-browsermenu";
@@ -32,12 +48,6 @@ in {
         desktopName = "Open Browser History";
         icon = "accessories-clock";
         exec = "${config.dotfiles.binDir}/rofi/browsermenu history";
-      })
-      (makeDesktopItem {
-        name = "rofi-open-file";
-        desktopName = "Open File ->";
-        icon = "folder";
-        exec = "${pkgs.rofi}/bin/rofi -show filebrowser";
       })
       (makeDesktopItem {
         name = "lock-display";
