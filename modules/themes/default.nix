c8865820344127f136f32d26e802eba93c324610
Inom Turdikulov <i@inomoz.ru>
2024-10-01 14:19:17 +0300

    Adjust cursor size

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -215,7 +215,7 @@ in {
           color-scheme = "prefer-dark";
           icon-theme = "${cfg.gtk.iconTheme}";
           cursor-theme = "${cfg.gtk.cursorTheme}";
-          cursor-size = 32;
+          cursor-size = 24;
       };
       home.configFile."qt5ct/qt5ct.conf".text = lib.generators.toINI { } qtctConf;
       home.configFile."qt6ct/qt6ct.conf".text = lib.generators.toINI { } qtctConf;
