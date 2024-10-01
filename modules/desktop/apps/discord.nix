deee18e1bfef4f4274a4d740b9a466d1da16d35c
Inom Turdikulov <i@inomoz.ru>
2024-10-01 23:50:02 +0300

    Fix inccorect module name

--- modules/desktop/apps/discord.nix
+++ modules/desktop/apps/discord.nix
@@ -2,9 +2,9 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.apps.vesktop;
+let cfg = config.modules.desktop.apps.discord;
 in {
-  options.modules.desktop.apps.vesktop = {
+  options.modules.desktop.apps.discord = {
     enable = mkBoolOpt false;
   };
 
