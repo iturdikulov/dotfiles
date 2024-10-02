e1de103bcde9260867df24ad9e3f635889e276b6
Inom Turdikulov <i@inomoz.ru>
2024-10-02 11:23:29 +0300

    Add more gamescope examples

--- modules/desktop/gaming/steam.nix
+++ modules/desktop/gaming/steam.nix
@@ -21,12 +21,22 @@ in {
       # Steam > Game > Properties > Launch Options and add:
       # run with resolution & fps & fullscreen & mangohud
       # gamescope -W 3840 -H 2160 -r 60 -f -e -- mangohud gamemoderun %command%
+      # gamescope -F fsr -w 1920 -h 1080 -W 3440 -H 1440 -b -r 60 -f -e -- mangohud gamemoderun %command%
       # Options:
       #   -W, --output-width             output width
       #   -H, --output-height            output height
+      #
+      #   -w, --nested-width             game width
+      #   -h, --nested-height            game height
+      #   set the resolution used by the game. If -h is specified but
+      #   -w isn't, a 16:9 aspect ratio is assumed. Defaults to the
+      #   values specified in -W and -H.
+      #
       #   -r, --nested-refresh           game refresh rate (frames per second)
       #   -e, --steam                    enable Steam integration
       #   -f, --fullscreen               make the window fullscreen
+      #   -b, --borderless               make the window borderless
+
       gamescope.enable = true;
 
       gamemode = {
