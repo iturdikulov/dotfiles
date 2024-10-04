19fe6af9eb2c5f7dcd818fbbf6b72aff2f21aaf5
Inom Turdikulov <i@inomoz.ru>
2024-10-04 20:23:57 +0300

    Drop not required TLP setting

--- hosts/ob/hardware-configuration.nix
+++ hosts/ob/hardware-configuration.nix
@@ -71,7 +71,6 @@
     # @see https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
     START_CHARGE_THRESH_BAT0=60;
     STOP_CHARGE_THRESH_BAT0=80;
-    RESTORE_THRESHOLDS_ON_BAT=1;
   };
 
   modules.hardware = {
