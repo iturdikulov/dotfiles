f0ed663d044191b32412f1b273cce540dd059949
Inom Turdikulov <i@inomoz.ru>
2024-10-04 20:18:20 +0300

    Adjust NNN plugins

--- modules/shell/files.nix
+++ modules/shell/files.nix
@@ -3,6 +3,13 @@
 with lib;
 with lib.my;
 let cfg = config.modules.shell.files;
+    configDir = config.dotfiles.configDir;
+    nnnPlugins = pkgs.fetchFromGitHub {
+          owner = "jarun";
+          repo = "nnn";
+          rev = "v5.0";
+          sha256 = "sha256-HShHSjqD0zeE1/St1Y2dUeHfac6HQnPFfjmFvSuEXUA=";
+        } + "/plugins";
 in {
   options.modules.shell.files = {
     enable = mkBoolOpt false;
@@ -37,17 +44,32 @@ in {
     ];
 
     # NNN plugins setup
-    env.NNN_PLUG = "i:imgview;d:dragdrop;D:dups;c:chksum;f:fzcd;F:fixname;m:mp3conv;o:oldbigfile;R:rsync;s:suedit";
+    env.NNN_PLUG = "i:imgview;d:dragdrop;D:dups;c:chksum;f:fzcd;F:fixname;m:mymount;M:mtpmount;o:oldbigfile;R:rsync;s:suedit";
     home.configFile = {
-      "nnn/plugins" = {
-        source = pkgs.fetchFromGitHub {
-          owner = "jarun";
-          repo = "nnn";
-          rev = "v4.9";
-          sha256 = "sha256-g19uI36HyzTF2YUQKFP4DE2ZBsArGryVHhX79Y0XzhU=";
-        } + "/plugins";
-        recursive = true;
-      };
+      # Service files, verify them when you change plugins revision
+      "nnn/plugins/.cbcp".source = nnnPlugins + "/.cbcp";
+      "nnn/plugins/.iconlookup".source = nnnPlugins + "/.iconlookup";
+      "nnn/plugins/.nmv".source = nnnPlugins + "/.nmv";
+      "nnn/plugins/.nnn-plugin-helper".source = nnnPlugins + "/.nnn-plugin-helper";
+      "nnn/plugins/.ntfy".source = nnnPlugins + "/.ntfy";
+
+      # My plugins
+      "nnn/plugins/mymount".source = "${configDir}/nnn/plugins/mymount";
+
+      # NNN plugins
+      "nnn/plugins/imgview".source = nnnPlugins + "/imgview";
+      "nnn/plugins/dragdrop".source = nnnPlugins + "/dragdrop";
+      "nnn/plugins/dups".source = nnnPlugins + "/dups";
+      "nnn/plugins/chksum".source = nnnPlugins + "/chksum";
+      "nnn/plugins/fzcd".source = nnnPlugins + "/fzcd";
+      "nnn/plugins/fixname".source = nnnPlugins + "/fixname";
+      "nnn/plugins/mtpmount".source = nnnPlugins + "/mtpmount";
+      "nnn/plugins/oldbigfile".source = nnnPlugins + "/oldbigfile";
+      "nnn/plugins/rsync".source = nnnPlugins + "/rsync";
+      "nnn/plugins/suedit".source = nnnPlugins + "/suedit";
     };
+
+    # Add nnn plugins to PATH
+    env.PATH = [ "${configDir}/nnn/plugins" ];
   };
 }
