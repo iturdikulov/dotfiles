4a2ce89edb80f213b57a7ad5a92c6b5d9b2d42fc
Inom Turdikulov <i@inomoz.ru>
2024-10-06 13:47:42 +0300

    Simplify tmux config, mosh integration

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -6,27 +6,34 @@ let cfg = config.modules.services.ssh;
 in {
   options.modules.services.ssh = {
     enable = mkBoolOpt false;
+    mosh.enable = mkBoolOpt true;
   };
 
-  config = mkIf cfg.enable {
-    services.openssh = {
-      enable = true;
-      settings = {
-        # Require keys over passwords. Ensure target machines are provisioned
-        # with authorizedKeys!
-        PasswordAuthentication = false;
+  config = mkIf cfg.enable (mkMerge [
+    {
+      services.openssh = {
+        enable = true;
+        settings = {
+          # Require keys over passwords. Ensure target machines are provisioned
+          # with authorizedKeys!
+          PasswordAuthentication = false;
+        };
+        # Suppress superfluous TCP traffic on new connections. Undo if using SSSD.
+        extraConfig = ''GSSAPIAuthentication no'';
+        # Deactive short moduli
+        moduliFile = pkgs.runCommand "filterModuliFile" {} ''
+          awk '$5 >= 3071' "${config.programs.ssh.package}/etc/ssh/moduli" >"$out"
+        '';
       };
-      # Suppress superfluous TCP traffic on new connections. Undo if using SSSD.
-      extraConfig = ''GSSAPIAuthentication no'';
-      # Deactive short moduli
-      moduliFile = pkgs.runCommand "filterModuliFile" {} ''
-        awk '$5 >= 3071' "${config.programs.ssh.package}/etc/ssh/moduli" >"$out"
-      '';
-    };
 
-    user.openssh.authorizedKeys.keys =
-      if config.user.name == "inom"
-      then ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ+98UauMXllELyhSNhTTJPITI2OmJSNf1HUXxjiv6V Inom M. Turdikulov inom@iturdikulov.org"]
-      else [];
-  };
+      user.openssh.authorizedKeys.keys =
+        if config.user.name == "inom"
+        then ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ+98UauMXllELyhSNhTTJPITI2OmJSNf1HUXxjiv6V Inom M. Turdikulov inom@iturdikulov.org"]
+        else [];
+    }
+
+    (mkIf cfg.mosh.enable {
+      programs.mosh.enable = true;
+    })
+  ]);
 }
