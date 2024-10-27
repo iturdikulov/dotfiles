e1b933891993b96d20a468e227ef0df41d0f1255
Inom Turdikulov <i@inomoz.ru>
2024-10-27 20:27:32 +0300

    Add ob scerets

---
+++ hosts/ob/secrets/secrets.nix
@@ -0,0 +1,7 @@
+let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ+98UauMXllELyhSNhTTJPITI2OmJSNf1HUXxjiv6V inom@iturdikulov.org";
+in {
+  "dns-env.age".publicKeys = [key];
+  "upsmon.age".publicKeys = [key];
+  "samba.age".publicKeys = [key];
+  "leetcode-cli.age".publicKeys = [key];
+}
