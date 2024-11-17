f1ba0e8984599bc129f736b1f991268d9d537ea8
Inom Turdikulov <i@inomoz.ru>
2024-11-17 10:50:27 +0300

    translation_script: modify alias, support multiline, detect language

--- modules/shell/trans.nix
+++ modules/shell/trans.nix
@@ -12,9 +12,31 @@ in {
     user.packages = with pkgs; [
       translate-shell
 
-      (writeScriptBin "tn" ''
+      # TODO: need to extract into separate script and simplify logic
+      (writeScriptBin "D" ''
         #!/bin/sh
-        ${getExe' pkgs.translate-shell "trans"} :ru -speak -join $@
+
+        if [[ $# -gt 0 ]]; then
+            CONTENT=$@
+            source=$(trans -id $CONTENT|sed -n 's/^Code *//p')
+        else
+            CONTENT=$(wl-paste -n)
+            source=$($CONTENT|trans -id|sed -n 's/^Code *//p')
+        fi
+
+        if [[ $source == *ru* ]]; then
+            target=en
+        else
+            target=ru
+        fi
+
+        echo Translation from $source to $target
+
+        if [[ $# -gt 0 ]]; then
+            ${getExe' pkgs.translate-shell "trans"} -brief -target $target -play -join $@
+        else
+            wl-paste -n|${getExe' pkgs.translate-shell "trans"} -brief -target $target -play -join $@
+        fi
        '')
     ];
   };
