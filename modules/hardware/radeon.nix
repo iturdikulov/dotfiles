ba82502823d3537281b6738c8d9b7e3d00c5dfa5
Inom Turdikulov <i@inomoz.ru>
2025-03-21 10:56:01 +0300

    radeon: refactoring, fix various issues with driver loading

--- modules/hardware/radeon.nix
+++ modules/hardware/radeon.nix
@@ -9,35 +9,31 @@ in {
   };
 
   config = mkIf cfg.enable {
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
+    hardware.amdgpu = {
+      initrd.enable = true;  #  enable loading amdgpu kernelModule in stage 1
+      opencl.enable = true;
     };
 
+    # Force enable RADV driver
+    environment.variables.AMD_VULKAN_ICD = "RADV";
+
     environment.systemPackages = with pkgs; [
-      vulkan-headers
       vulkan-loader
-      vulkan-tools
+
+      vulkan-headers
+      vulkan-tools  # vulkaninfo, vkcube, vkcubepp
+
       corectrl
-      clinfo  # OpenCL info
+      clinfo
+      amdgpu_top
     ];
 
     services.xserver.videoDrivers = [ "amdgpu" ];
     services.xserver.deviceSection = ''Option "TearFree" "true"'';  # Fix tearing for amdgpu.
 
     hardware.graphics.extraPackages = with pkgs; [
-      clinfo
-      amdvlk
       rocmPackages.clr.icd
     ];
-
-    # For 32 bit applications
-    hardware.graphics.extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk
-    ];
-
-    # Force radv
-    environment.variables.AMD_VULKAN_ICD = "RADV";
 
     # Most software has the HIP libraries hard-coded. You can work around it on NixOS by using
     systemd.tmpfiles.rules = [
