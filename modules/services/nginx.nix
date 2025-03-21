7f3d9100ac1b738ed7961dcace001c31b7c12557
Inom Turdikulov <i@inomoz.ru>
2025-03-21 10:58:37 +0300

    ngnix: use default config

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -29,21 +29,9 @@ in {
         # of buffer overflow attacks. This can be tweaked on a per-vhost basis,
         # as needed.
         clientMaxBodySize = "256k";  # default 10m
+
         # Significantly speed up regex matchers
         appendConfig = ''pcre_jit on;'';
-        commonHttpConfig = ''
-          client_body_buffer_size  4k;       # default: 8k
-          large_client_header_buffers 2 4k;  # default: 4 8k
-
-          map $sent_http_content_type $expires {
-              default                    off;
-              text/html                  10m;
-              text/css                   max;
-              application/javascript     max;
-              application/pdf            max;
-              ~image/                    max;
-          }
-        '';
       };
     })
 
