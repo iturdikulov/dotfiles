b5536db3d42fb04b758a5c317a4a573755e50abb
Inom Turdikulov <i@inomoz.ru>
2025-03-22 23:20:46 +0300

    Disable experimental features for bluetooth

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -13,11 +13,6 @@ in {
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
-      settings = {
-        General = {
-          Experimental = true;  # Enable Battery level reporting
-        };
-      };
     };
   };
 }
