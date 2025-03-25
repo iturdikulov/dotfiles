8b2c1f36c9ba1016a3b65a209bf4bd21821ffde8
Inom Turdikulov <i@inomoz.ru>
2025-03-25 22:34:08 +0300

    Drop not required sevice to track timewarrior state

--- modules/desktop/term/task.nix
+++ modules/desktop/term/task.nix
@@ -16,33 +16,10 @@ in {
     user.packages = with pkgs; [
       taskwarrior3 # my daily tasks
       timewarrior # time tracking
+      taskwarrior-tui
     ];
 
     home.configFile."task/taskrc".source = "${configDir}/task/taskrc";
     modules.shell.zsh.rcFiles = [ "${configDir}/task/aliases.zsh" ];
-
-    # NEXT: wrap into own nix module
-    # Notify me when timewarrior is tracking
-    systemd.timers."timew_track" = {
-      wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "25m";
-        OnUnitActiveSec = "25m";
-        Unit = "timew_track.service";
-      };
-    };
-
-    systemd.services."timew_track" = {
-      script = ''
-          # DBUS is optimized for dwm-desktop, you need to tune it for your DE
-          export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u $USER)/bus
-          export DISPLAY=:0
-          $(${pkgs.timewarrior}/bin/timew|grep Tracking && ${pkgs.libnotify}/bin/notify-send --urgency=low --expire-time=1000 "Timewarrior" "Tracking is active") || exit 0
-      '';
-      serviceConfig = {
-        Type = "oneshot";
-        User = config.user.name;
-      };
-    };
   };
 }
