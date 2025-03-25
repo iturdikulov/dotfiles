5d873860c202ed7cee03997509fad324ca861793
Inom Turdikulov <i@inomoz.ru>
2025-03-25 22:58:50 +0300

    graphics: add 3d printing software, optimize blender settings

--- modules/desktop/media/graphics.nix
+++ modules/desktop/media/graphics.nix
@@ -26,44 +26,48 @@ in {
       # @see https://github.com/edolstra/nix-warez/tree/master/blender
       nixpkgs.overlays = [ inputs.blender-bin.overlays.default ];
 
-      # 3D modelling
+      # 3D modelling & 3D printing
       user.packages = with pkgs; [
           f3d                   # Fast and minimalist 3D viewer using VTK
           unstable.freecad-wayland  # General purpose Open Source 3D CAD/MCAD/CAx/CAE/PLM modeler
+          openscad-unstable
           # TODO: verify libcrypt-legacy is required
           # Blender itself doesn't need libxcrypt-legacy, but I use blenderkit,
           # which needs libcrypt.so.1, which libxcrypt no longer provides.
           pkgs."blender_${builtins.replaceStrings ["."] ["_"] blenderVersion}"
           solvespace
           fspy                  # Quick and easy still image camera matching
-          my.tungsten              # 3D rendering engine
-      ];
+        ];
+
+      services.flatpak.enable = true;
+
+      # OpenSCAD libraries
+      home.dataFile."OpenSCAD/libraries/Round-Anything" = {
+        source = pkgs.fetchFromGitHub {
+          owner = "Irev-Dev";
+          repo = "Round-Anything";
+          rev = "refs/heads/master";
+          hash = "sha256-xED5fz+VmVv4VJO72PQuExqOtxFdCQ4v4GA99GvA70E=";
+        };
+        recursive = true;
+      };
 
       # I copy these files manually because they should be mutable, as Blender is
       # very stateful. Having a consistent starting point for new systems is good
       # enough for me.
       system.userActivationScripts.setupBlenderConfig = ''
-        # Copy config
-        destdir="$XDG_CONFIG_HOME/blender/${blenderVersion}/config"
-        mkdir -p "$destdir"
-        for cfile in ${configDir}/blender/config/*; do
-          basename="$(basename $cfile)"
-          dest="$destdir/$basename"
-          if [ ! -e "$dest" ]; then
-            cp "$cfile" "$dest"
-          fi
-        done
-        for bfile in startup userpref; do
-          src="${configDir}/blender/$bfile.blend.tar.gz"
-          if [ ! -e "$destdir/$bfile.blend" ]; then
-            ${pkgs.gnutar}/bin/tar xzvf "$src" -C "$destdir"
-          fi
-        done
-
-        # Copy scripts
+        src="${configDir}/blender/initial_configuration.7z"
         destdir="$XDG_CONFIG_HOME/blender/${blenderVersion}"
-        src="${configDir}/blender/config.tar.gz"
-        ${pkgs.gnutar}/bin/tar xzvf "$src" -C "$destdir"
+
+        # Exit if the config directory already exists
+        if [ -d "$destdir" ]; then
+            echo "Config directory already exists, skipping setup"
+            exit 0
+        fi
+
+        # Extract the config directory with startup files
+        mkdir -p "$destdir"
+        ${getExe' pkgs._7zz "7zz"} x "$src" -o"$destdir"
       '';
     })
 
@@ -75,17 +79,8 @@ in {
 
           image_optim    # Optimize images using multiple utilities
           tesseract4     # OCR engine
-          maim           # A command-line screenshot utility
+          gImageReader   # Simple Gtk/Qt front-end to tesseract-ocr
           flameshot      # Screenshots
-
-          # TODO: rewrite this, broken!
-          (pkgs.writeScriptBin "scrcap_ocr" ''
-          #!/bin/sh
-          maim -s | convert - -units PixelsPerInch -resample 300 -sharpen 12x6.0 - \
-          | tesseract -l eng+rus stdin stdout \
-          | wl-copy
-          notify-send "Screenshot OCR" "Image copied to clipboard"
-          '')
         ] else []) ++
 
         # Replaces Illustrator (maybe indesign?)
@@ -147,12 +142,14 @@ in {
         # Photography workflow
         (if cfg.photos.enable then [
           darktable
+          imv
         ] else []) ++
 
         # Video editing
         (if cfg.videos.enable then [
           losslesscut-bin
-          kdenlive
+          kdePackages.kdenlive
+          davinci-resolve
         ] else []);
 
       home.configFile = mkIf cfg.raster.enable {
