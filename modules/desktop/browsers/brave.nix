a5f1500b47f7709847965f4d9ca19d6cca249b25
Inom Turdikulov <i@inomoz.ru>
2025-04-05 00:06:40 +0300

    Switch to x11 for brave, to temporarily fix screen sharing

--- modules/desktop/browsers/brave.nix
+++ modules/desktop/browsers/brave.nix
@@ -7,6 +7,13 @@
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.browsers.brave;
+    bravePkg =
+  (if cfg.proxy.enable
+    then
+    (pkgs.brave.override {
+      commandLineArgs = "--proxy-server=\"${config.networking.globalProxy.host}:${config.networking.globalProxy.port}\"";
+    })
+  else pkgs.brave );
 in {
   options.modules.desktop.browsers.brave = {
     enable = mkBoolOpt false;
@@ -15,19 +22,22 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      (if cfg.proxy.enable
-        then
-          (brave.override {
-            commandLineArgs = "--proxy-server=\"${config.networking.globalProxy.host}:${config.networking.globalProxy.port}\"";
-          })
-      else brave )
+
+      (makeDesktopItem {
+        name = "brave-x11";
+        desktopName = "Brave Web Browser x11";
+        genericName = "Open a Brave window x11";
+        icon = "brave";
+        exec = "env XDG_SESSION_TYPE=x11 ${bravePkg}/bin/brave";
+        categories = [ "Network" ];
+      })
 
       (makeDesktopItem {
         name = "brave-private";
         desktopName = "Brave Web Browser";
         genericName = "Open a private Brave window";
         icon = "brave";
-        exec = "${brave}/bin/brave --incognito";
+        exec = "${bravePkg}/bin/brave --incognito";
         categories = [ "Network" ];
       })
     ];
