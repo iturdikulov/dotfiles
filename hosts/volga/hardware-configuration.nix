f92c496bc80fe67f36aa609a2061162905bee85d
Inom Turdikulov <i@inomoz.ru>
2025-04-11 23:53:40 +0300

    Add u2f token support for swaylock

--- hosts/volga/hardware-configuration.nix
+++ hosts/volga/hardware-configuration.nix
@@ -64,6 +64,24 @@ options libata          allow_tpm=0
     sudo.u2fAuth = true;
   };
 
+  security.pam.services.swaylock.text = ''
+    # Account management.
+    account required pam_unix.so
+
+    # Authentication management.
+    auth sufficient pam_unix.so nullok likeauth try_first_pass
+    auth sufficient ${pkgs.pam_u2f}/lib/security/pam_u2f.so
+    auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
+    auth required pam_deny.so
+
+    # Password management.
+    password sufficient pam_unix.so nullok sha512
+
+    # Session management.
+    session required pam_env.so conffile=/etc/pam/environment readenv=0
+    session required pam_unix.so
+  '';
+
   # Custom hardware options
   modules.hardware = {
     audio.enable = true;
