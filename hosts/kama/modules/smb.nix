fb7aeee4c3578a0a6ccb00eeaf0fdd5c08866927
Inom Turdikulov <i@inomoz.ru>
2025-04-12 23:17:26 +0300

    Adjust samba config

--- hosts/kama/modules/smb.nix
+++ hosts/kama/modules/smb.nix
@@ -5,18 +5,15 @@
     enable = true;
     securityType = "user";
     openFirewall = true;
-    settings = ''
-      workgroup = WORKGROUP
-      server string = ${config.networking.hostName}
-      netbios name = ${config.networking.hostName}
-      security = user
-      hosts allow = 100.87.0. 192.168.0. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-      '';
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+      };
+    };
     shares = {
-      public = {
+      media = {
         path = "/media/";
         browseable = "yes";
         "read only" = "no";
