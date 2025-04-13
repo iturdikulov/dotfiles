b03937f92fd9d316ffded5f99db10b4531e3f40c
Inom Turdikulov <i@inomoz.ru>
2025-04-13 04:16:14 +0300

    Add singbox secrets

--- hosts/kama/secrets/secrets.nix
+++ hosts/kama/secrets/secrets.nix
@@ -1,3 +1,7 @@
 let key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ+98UauMXllELyhSNhTTJPITI2OmJSNf1HUXxjiv6V inom@iturdikulov.org";
 in {
+  "singbox_vless_server.age".publicKeys = [key];
+  "singbox_vless_uuid.age".publicKeys = [key];
+  "singbox_vless_public_key.age".publicKeys = [key];
+  "singbox_vless_short_id.age".publicKeys = [key];
 }
